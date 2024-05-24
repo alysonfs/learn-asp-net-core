0e140105d05aeb359d8db533c4ee10d8c9a87a37
Alyson F Souza <alysonforever@gmail.com>
2024-05-24 17:25:46 -0300

    chore: Update RazorPagesMovie pages to include search functionality and filter by genre

--- RazorPagesMovie/Pages/Movies/Index.cshtml.cs
+++ RazorPagesMovie/Pages/Movies/Index.cshtml.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.EntityFrameworkCore;
 using RazorPagesMovie.Data;
 using RazorPagesMovie.Models;
@@ -21,9 +22,36 @@ namespace RazorPagesMovie.Pages.Movies
 
         public IList<Movie> Movie { get;set; } = default!;
 
+        [BindProperty(SupportsGet = true)]
+        public string? SearchString { get; set; }
+
+        public SelectList? Genres { get; set; }
+
+        [BindProperty(SupportsGet = true)]
+        public string? MovieGenre { get; set; }
+
         public async Task OnGetAsync()
         {
-            Movie = await _context.Movie.ToListAsync();
+            // Use LINQ to get list of genres.
+            IQueryable<string> genreQuery = from genre in _context.Movie
+                                            orderby genre.Genre
+                                            select genre.Genre;
+
+            var movies = from movie in _context.Movie 
+                                            select movie;
+            
+            if(!string.IsNullOrEmpty(SearchString))
+            {
+                movies = movies.Where(s => s.Title.Contains(SearchString));
+            }
+
+            if(!string.IsNullOrEmpty(MovieGenre))
+            {
+                movies = movies.Where(x => x.Genre == MovieGenre);
+            }
+
+            Genres = new SelectList(await genreQuery.Distinct().ToListAsync());
+            Movie = await movies.ToListAsync();
         }
     }
 }
