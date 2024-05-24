ecc26aa640d233cb2ca8b78492e0387da86ec404
Alyson F Souza <alysonforever@gmail.com>
2024-05-24 16:31:07 -0300

    chore: Add SeedData class to initialize movie data in RazorPagesMovie project

--- RazorPagesMovie/Program.cs
+++ RazorPagesMovie/Program.cs
@@ -1,6 +1,7 @@
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.DependencyInjection;
 using RazorPagesMovie.Data;
+using RazorPagesMovie.Models;
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -9,6 +10,12 @@ builder.Services.AddDbContext<RazorPagesMovieContext>(options =>
     options.UseSqlite(builder.Configuration.GetConnectionString("RazorPagesMovieContext") ?? throw new InvalidOperationException("Connection string 'RazorPagesMovieContext' not found.")));
 
 var app = builder.Build();
+
+using ( var scope = app.Services.CreateScope())
+    {
+        var services = scope.ServiceProvider;
+        SeedData.Initialize(services);
+    }
 
 // Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
