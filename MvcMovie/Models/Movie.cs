1a8a81cf127a744ec4e6e0a21f4abf49acd6d53e
Alyson F Souza <alysonforever@gmail.com>
2024-05-28 08:39:20 -0300

    feat: Add search functionality to Movies index page

--- MvcMovie/Models/Movie.cs
+++ MvcMovie/Models/Movie.cs
@@ -1,4 +1,5 @@
 using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace MvcMovie.Models;
 
@@ -7,10 +8,12 @@ public class Movie
     public int ID { get; set; }
     public string? Title { get; set; }
 
+    [Display(Name = "Release Date")]
     [DataType(DataType.Date)]
     public DateTime ReleaseDate { get; set; }
     
     public string? Genre { get; set; }
     
+    [Column(TypeName = "decimal(18, 2)")]
     public decimal Price { get; set; }
 }
